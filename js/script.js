65dbfeef67f30d57aea5919397a652e53f4389ec
Roydano <roy.dano90@gmail.com>
2021-06-08 08:02:51 +0200

    add codice pulito + soluzione palindromi con ciclo for

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 // Palidroma Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
 
+
+//****** metodo javascript ******/
 var parola = prompt('Inserisci una parola');
-var paliOrNot = palindromo(parola);
-if(paliOrNot == true){
+if(palindromo(parola)){
     document.getElementById('palindromo').innerHTML = parola + ' è palindromo';
 } else{
     document.getElementById('palindromo').innerHTML = parola + ' non è palindromo';
+}
+
+//****** metodo ciclo for ******/
+var parola2 = prompt('Inserisci una parola');
+if(palinFor(parola2)){
+    document.getElementById('palinfor').innerHTML = parola2 + ' è palindromo';
+} else{
+    document.getElementById('palinfor').innerHTML = parola2 + ' non è palindromo';
 }
 
 
@@ -13,16 +22,11 @@ if(paliOrNot == true){
 // Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 var pariDispari = prompt('pari o dispari?');
-
 var userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
 
-var pcNumber = randomNum(1, 5);
+var somma = userNumber + randomNum(1, 5);
 
-var somma = userNumber + pcNumber;
-
-var winner = pariOrNot(somma);
-
-if(winner == true && pariDispari == 'pari' || winner == false && pariDispari == 'dispari'){
+if(isEven(somma) && pariDispari == 'pari' || !isEven(somma) && pariDispari == 'dispari'){
     document.getElementById('pari-dispari').innerHTML = 'Hai vinto!';
 
 }else{
@@ -40,7 +44,24 @@ function palindromo(word){
     if(word == word.split('').reverse().join('')){
         return true;
     }
+    return false;
 }
+
+//parola palindroma con ciclo for...in questo caso il metodo prende la parola partendo dalla fine e analizzando le lettere
+function palinFor(word){
+    var wordReverse = '';
+
+    for(var i = word.length - 1; i >= 0; i--){
+        wordReverse += word[i];
+    }
+
+    if(word == wordReverse){
+        return true;
+    }
+    return false;
+}
+
+
 
 
 // generare un numero random
@@ -50,8 +71,9 @@ function randomNum(min, max){
 
 
 // stabilire se la somma è pari o dispari
-function pariOrNot(num){
+function isEven(num){
     if(num % 2 == 0){
         return true;
     }
+    return false;
 }
